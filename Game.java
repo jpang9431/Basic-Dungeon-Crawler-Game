f5c5a90006c6c636b30d3c2c7cf3d4f5457cd079
jpang9431 <69761561+jpang9431@users.noreply.github.com>
2023-12-01 02:56:49 +0000

    Finished the user interface of battle going to work on the funcionality

--- Game.java
+++ Game.java
@@ -1,8 +1,14 @@
+import javax.swing.*;
 class Game{
 	public final static SkillDict dict = new SkillDict();
 	public final static int width = 800, height = 450;
+	private JFrame frame = new JFrame("Game");
 	Game(){
-		System.out.println("WIP");
+		TestUser user = new TestUser();
+		Slime slime2 = new Slime();
+		frame.setContentPane(new Battle(user, slime2));
+		frame.setSize(width, height);
+		frame.setVisible(true);
 	}
 	public static void main(String[] args) {
 		new Game();
--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@
 
 class Main {
   public static void main(String[] args) {
-    
+    new Game();
 		
 		
   }
